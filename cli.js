235667da0740b9bae8119af67e60af136e1a2b55
Ira Miller <public@iramiller.com>
2018-09-04 15:59:58 -0500

    upgrade deps

--- cli.js
+++ cli.js
@@ -1,16 +1,18 @@
 #!/usr/bin/env node
 const strReplace = require('guld-fs-str-replace')
 const program = require('commander')
-const VERSION = require('./package.json').version
+const thispkg = require(`${__dirname}/package.json`)
+const runCLI = require('guld-cli-run')
 
 /* eslint-disable no-console */
 program
-  .name('guld-fs-str-replace')
+  .name(thispkg.name.replace('-cli', ''))
+  .version(thispkg.version)
+  .description(thispkg.description)
   .usage('<path> <old-str> <new-str> [flags...]', 'Replace old-string with new-string for all files in path.')
-  .description('Replace old-string with new-string for all files in path.')
-  .version(VERSION)
   .action(async (p, o, n, flags) => {
     strReplace(o, n, [p].concat(flags.rawArgs.slice(3)))
   })
 
-program.parse(process.argv)
+runCLI.bind(program)()
+module.exports = program
